44fce9b595698cd51279d8b114672d48ed901d83
Max Gordon <gordonmaxc@gmail.com>
2015-02-18 16:27:02 -0800

    style

--- Ember_Travel_Journal/app/controllers/posts/edit.js
+++ Ember_Travel_Journal/app/controllers/posts/edit.js
@@ -2,14 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    updatePost: function  (post) {
+    updatePost: function  () {
     this.get('model').save()
     },
   },
-
-  resetForm: function  () {
-    this.set("name", "")
-    this.set("description", "")
-    this.set("photo", "")
-  }
 });
